68bbb87245672155261a608d989a4e770c87dad0
Yurii Yakubin <yurii.yakubin@gmail.com>
2025-03-29 17:59:33 +0000

    Move __createGoalList to GlobalContext

--- src/bitmake/GlobalContext.js
+++ src/bitmake/GlobalContext.js
@@ -1,13 +1,20 @@
 "use strict";
 
+const path = require("node:path");
 const fs = require("node:fs");
 
+const { AbsolutePath } = require("###/utils/AbsolutePath.js");
 const { fileExistsSync } = require("###/utils/FileSystem.js");
 const { TargetCollection } = require("./TargetCollection.js");
 const { ScriptCollection } = require("./ScriptCollection.js");
 const { InterfaceIncludes } = require("./InterfaceIncludes.js");
 const { InterfaceTarget } = require("./InterfaceTarget.js");
 const { IncludeDirectory } = require("./IncludeDirectory.js");
+const { GoalCollection } = require("./GoalCollection.js");
+const { InterfaceObjects } = require("./InterfaceObjects.js");
+const { SourceFile } = require("./SourceFile.js");
+const { ObjectLibrary, StaticLibrary, SharedLibrary, Executable } = require("./Target.js");
+const { FilePath, DirPath } = require("./Path.js");
 
 const TARGETS = Symbol("TARGETS");
 const SCRIPTS = Symbol("SCRIPTS");
@@ -163,6 +170,186 @@ GlobalContext.prototype.getAllLibraries = function(target) {
   const targetSet = new Set([ target.NAME ]);
   this.__getAllLibraries(libraries, targetSet, target.getLibraries());
   return libraries;
+}
+
+function scopeValueAsPrimitives(o) {
+  if (typeof o === "undefined")
+    return o;
+  if (typeof o === "boolean")
+    return o;
+  if (typeof o === "number")
+    return o;
+  if (typeof o === "string")
+    return o;
+  if (typeof o === "object") {
+    if (!o)
+      return o;
+    if (o instanceof AbsolutePath) {
+      return o.toString();
+    }
+    if (o instanceof Array) {
+      const result = [];
+      for (const i of o)
+        result.push(scopeValueAsPrimitives(i));
+      return result;
+    }
+    if (o instanceof Object) {
+      const result = {};
+      for (const [k,v] of Object.entries(o))
+        result[k] = scopeValueAsPrimitives(v);
+      return result;
+    }
+  }
+  throw new Error(`Unknown instance of ${o}`);
+}
+
+GlobalContext.prototype.createGoals = function(scope) {
+  const goalList = GoalCollection.create();
+  for (const iter of Object.values(this[INTERFACE_TARGETS])) {
+    const target = this[TARGETS].get(iter.NAME);
+    target.addSources(iter.SOURCES);
+    for (const it of iter.INCLUDES) {
+      if (it.PUBLIC_ONLY)
+        target.addPublicIncludes(it.VALUE);
+      else
+        target.addIncludes(it.VALUE);
+    }
+  }
+
+  for (const iter of Object.values(this[INTERFACE_SCRIPTS])) {
+    const script = this[SCRIPTS].get(iter.NAME);
+    for (const [key, vals] of Object.entries(iter.PROPERTIES))
+      script.addProperty(key, ...vals);
+  }
+
+  for (const [name, script] of Object.entries(this[SCRIPTS].ENTRIES)) {   
+    const depends = [ script.FILE.toString() ];
+    if (script.INPUT)
+      depends.push(script.INPUT.toString());
+    const msg = "\x1b[36m" + "Generating " + script.TARGET_SCOPE.BINARY_DIR.relative(script.OUTPUT) + "\x1b[0m";
+    const params = { ...script.PROPERTIES, ...script.PARAMS };
+    goalList.addScript(script.FILE, "", depends, script.OUTPUT.toString(), scopeValueAsPrimitives(params), msg);
+  }
+
+  for (const [name, target] of Object.entries(this[TARGETS].ENTRIES)) {
+    const headers = this.getAllHeaders(target);
+    const depends = [];
+    for (const s of target.SOURCES) {
+      if (s instanceof InterfaceObjects) {
+        const t = this[TARGETS].get(s.NAME);
+        for (const f of t.SOURCES) {
+          if (f instanceof SourceFile && f.OBJECT_FILE)
+            depends.push(f.OBJECT_FILE.toString());
+        }
+        continue;
+      }
+
+      if (s.HEADER_FILE_ONLY)
+        continue;
+
+      fs.mkdirSync(s.OBJECT_FILE_DIR.toString(), { recursive: true });
+
+      const relativeObject = target.TARGET_SCOPE.BINARY_DIR.relative(s.OBJECT_FILE);
+      const relativeBinaryDir = scope.PROJECT_BINARY_DIR.relative(target.TARGET_SCOPE.BINARY_DIR);
+      const msg = "\x1b[32m" + `Building ${s.LANGUAGE} object ${relativeBinaryDir}/${relativeObject}` + "\x1b[0m";
+
+      const args = [];
+      args.push(...target.TARGET_SCOPE[s.LANGUAGE + "_FLAGS"]);
+      args.push(...target.TARGET_SCOPE[s.LANGUAGE + "_FLAGS_" + target.TARGET_SCOPE.BUILD_TYPE.toUpperCase()]);
+      args.push(...target.COMPILE_OPTIONS);
+      args.push(...s.COMPILE_FLAGS);
+      args.push(...this.getAllIncludes(target).map(i => "-I" + i));
+      args.push("-o", relativeObject);
+      args.push("-c", s.FILE);
+      const cwd = target.TARGET_SCOPE.BINARY_DIR.toString();
+
+      const command = target.TARGET_SCOPE[s.LANGUAGE + "_COMPILER"].toString();
+      const output = target.TARGET_SCOPE.BINARY_DIR.join(relativeObject).toString();
+      depends.push(output);
+
+      goalList.addExec(output, [ ...headers, s.FILE ], command, args, cwd, msg);
+    }
+
+    if (target instanceof ObjectLibrary) {
+      const objs = depends.filter(i => i.endsWith(".o") || i.endsWith(".obj")).map(i => target.FILE_DIR.relative(i));
+      if (objs.length) {
+        const args = [ ...target.LINK_OPTIONS, "-r", "-o", target.FILE_NAME, ...objs ];
+        const cwd = target.FILE_DIR.toString();
+        const msg = `Linking CXX object library ${target.FILE_NAME}`;
+        goalList.addExec(target.FILE.toString(), depends, scope.LINKER, args, cwd, msg);
+      }
+      else {
+        console.log(`No objects for "${target.NAME}"`);
+      }
+    }
+
+    if (target instanceof StaticLibrary) {
+      const objs = depends.filter(i => i.endsWith(".o") || i.endsWith(".obj")).map(i => target.FILE_DIR.relative(i));
+      if (objs.length) {
+        const args = [ "rc", target.FILE_NAME , ...objs ];
+        const cwd = target.FILE_DIR.toString();
+        const msg = `Linking CXX static library ${target.FILE_NAME}`;
+        goalList.addExec(target.FILE.toString(), depends, scope.AR, args, cwd, msg);
+      }
+      else {
+        console.log(`No objects for "${target.NAME}"`);
+      }
+    }
+
+    if (target instanceof SharedLibrary) {
+      throw new Error("Not implemented");
+    }
+
+    if (target instanceof Executable) {
+      const objs = depends.filter(i => i.endsWith(".o") || i.endsWith(".obj")).map(i => target.FILE_DIR.relative(i));
+      if (objs.length) {
+        const libs = this.getAllLibraries(target);
+        const args = [
+          ...target.TARGET_SCOPE.CXX_FLAGS,
+          ...target.LINK_OPTIONS,
+          ...objs,
+          "-o", target.FILE_NAME,
+          ...libs.map(i => target.FILE_DIR.relative(i)),
+        ];
+        const cwd = target.FILE_DIR.toString();
+        const msg = `Linking CXX executable ${target.FILE_NAME}`;
+        goalList.addExec(target.FILE.toString(), depends.concat(libs), scope.CXX_COMPILER, args, cwd, msg);
+      }
+      else {
+        console.log(`No objects for "${target.NAME}"`);
+      }
+    }
+
+    goalList.addTarget(name, [ target.FILE.toString() ], `Built target ${name}`);
+  }
+
+  const install_files = [];
+  const install_script = path.posix.join(__dirname, "SystemScripts/install_script.js");
+  for (const iter of this[INSTALL_LIST]) {
+    let src, dest;
+    if (iter.VALUE instanceof FilePath) {
+      src = iter.VALUE.toString();
+      const rfile = iter.BASE_DIR.relative(iter.VALUE);
+      dest = iter.DESTINATION.join(rfile);
+    }
+    else if (iter.VALUE instanceof InterfaceTarget) {
+      const target = this[TARGETS].get(iter.VALUE.NAME);
+      src = target.FILE.toString();
+      dest = iter.DESTINATION.join(target.FILE_NAME);
+    }
+    else {
+      throw new Error(`Can not install ${iter.VALUE}`)
+    }
+    if (scope.DESTDIR)
+      dest = DirPath.create(scope.DESTDIR).join(dest);
+    goalList.addScript(install_script, "", [ src ], dest, scopeValueAsPrimitives({src, dest}), "");
+    install_files.push(dest);
+  }
+
+  goalList.addTarget("install", install_files, "");
+  goalList.addTarget("all", Object.keys(this[TARGETS].ENTRIES), "");
+
+  return goalList;
 }
 
 module.exports = {

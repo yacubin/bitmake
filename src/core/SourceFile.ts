a93941f0827eed9d60d33458185bfdcda5772554
Yurii Yakubin <yurii.yakubin@gmail.com>
2025-07-19 18:59:36 +0100

    SourceFile.toJSON

--- src/core/SourceFile.ts
+++ src/core/SourceFile.ts
@@ -7,34 +7,26 @@
  * under the MIT License. See LICENSE file for details.
  */
 
-import { ensureBoolean } from "@/utils/StrictType";
 import { AbsolutePath } from "@/core/AbsolutePath";
-
-const LANGUAGE         = Symbol("LANGUAGE");
-const HEADER_FILE_ONLY = Symbol("HEADER_FILE_ONLY");
-const DEFINES          = Symbol("DEFINES");
-const COMPILE_PATH     = Symbol("COMPILE_PATH");
-const COMPILE_FLAGS    = Symbol("COMPILE_FLAGS");
-const FILE             = Symbol("FILE");
-const BASE_DIR         = Symbol("BASE_DIR");
+import { SimpleObject } from "@/core/SimpleObject";
 
 export class SourceFile {
-  private [LANGUAGE]: string;
-  private [HEADER_FILE_ONLY]: boolean;
-  private [FILE]: AbsolutePath;
-  private [BASE_DIR]: AbsolutePath;
-  private [DEFINES]: string[];
-  private [COMPILE_PATH]: string;
-  private [COMPILE_FLAGS]: Array<string|string[]>;
+  private _language: string;
+  private _headerOnly: boolean;
+  private _filename: AbsolutePath;
+  private _baseDir: AbsolutePath;
+  private _definitions: string[];
+  private _compilePath: string;
+  private _compileOptions: Array<string | string[]>;
 
   private constructor(filename: AbsolutePath, baseDir: AbsolutePath, language: string, compilerPath: string, compileFlags: Array<string | string[]>) {
-    this[FILE] = filename;
-    this[BASE_DIR] = baseDir;
-    this[LANGUAGE] = language;
-    this[HEADER_FILE_ONLY] = !language;
-    this[DEFINES] = [];
-    this[COMPILE_PATH] = compilerPath;
-    this[COMPILE_FLAGS] = [ ...compileFlags ];
+    this._filename = filename;
+    this._baseDir = baseDir;
+    this._language = language;
+    this._headerOnly = !language;
+    this._definitions = [];
+    this._compilePath = compilerPath;
+    this._compileOptions = [ ...compileFlags ];
   }
 
   public static create(filename: AbsolutePath, baseDir: AbsolutePath, language: string, compilerPath: string, compileFlags: Array<string | string[]>) {
@@ -42,56 +34,59 @@ export class SourceFile {
   }
 
   public get LANGUAGE(): string {
-    return this[LANGUAGE];
+    return this._language;
   }
 
   public get HEADER_FILE_ONLY(): boolean {
-    return this[HEADER_FILE_ONLY];
-  }
-
-  public set HEADER_FILE_ONLY(value: boolean) {
-    this[HEADER_FILE_ONLY] = ensureBoolean(value);
+    return this._headerOnly;
   }
 
   public get DEFINES(): string[] {
-    return this[DEFINES]
+    return this._definitions;
   }
 
   public addDefinition(definition: string) {
-    this[DEFINES].push(definition);
+    this._definitions.push(definition);
   }
 
   public get COMPILE_PATH(): string {
-    return this[COMPILE_PATH];
+    return this._compilePath;
   }
 
   public get COMPILE_FLAGS(): Array<string | string[]> {
-    return this[COMPILE_FLAGS];
+    return this._compileOptions;
   }
 
   public get FILE(): AbsolutePath {
-    return this[FILE];
+    return this._filename;
   }
 
   public get FILE_DIR(): AbsolutePath {
-    return this[FILE].dirname();
+    return this._filename.dirname();
   }
 
   public get FILE_NAME(): string {
-    return this[FILE].basename();
+    return this._filename.basename();
   }
 
-  public toJSON(): object {
-    return {
-      LANGUAGE: this[LANGUAGE],
-      HEADER_FILE_ONLY: this[HEADER_FILE_ONLY],
-      DEFINES: this[DEFINES],
-      COMPILE_PATH: this[COMPILE_PATH],
-      COMPILE_FLAGS: this[COMPILE_FLAGS],
-      FILE: this[FILE],
-      FILE_DIR: this.FILE_DIR,
-      FILE_NAME: this.FILE_NAME,
-      BASE_DIR: this[BASE_DIR],
+  public toJSON(): SimpleObject {
+    const json: SimpleObject = {
+      type: SourceFile.name,
+      filename: this._filename,
+      baseDir: this._baseDir,
     };
+    if (this._headerOnly)
+      json.headerOnly = this._headerOnly;
+    else {
+      if (this._language)
+        json.language = this._language;
+      if (this._definitions.length)
+        json.definitions = this._definitions;
+      if (this._compilePath)
+        json.compilePath = this._compilePath;
+      if (this._compileOptions.length)
+        json.compileOptions = this._compileOptions;
+    }
+    return json;
   }
 }

4a6ee818218a6e724ff8eda388d0d7fd6578cbe6
Yurii Yakubin <yurii.yakubin@gmail.com>
2025-07-23 22:58:41 +0100

    Replaced spawnSync to spawnAsync

--- src/core/SpawnSyncTask.ts
+++ src/core/SpawnSyncTask.ts
@@ -7,10 +7,9 @@
  * under the MIT License. See LICENSE file for details.
  */
 
-import { spawnSync } from "node:child_process";
-
 import { InterfaceTask } from "@/core/MakeInterfaces";
 import { SimpleObject } from "@/core/SimpleObject";
+import { spawnAsync } from "@/utils/ChildProcess";
 import { Logger } from "@/logger";
 
 const logger = Logger.create(import.meta.url);
@@ -34,14 +33,14 @@ export class SpawnSyncTask extends InterfaceTask {
     this._cwd = cwd;
   }
 
-  public execute(): void {
-    logger.debug("spawnSync");
+  public async execute(): Promise<void> {
+    logger.debug("spawnAsync");
     logger.debug("  command", this._command);
     logger.debug("  cwd", this._cwd);
     for (let i = 0; i < this._args.length; i++)
       logger.debug(`  args[${i}]`, this._args[i]);
 
-    const result = spawnSync(this._command, this._args, { cwd: this._cwd, encoding: "utf-8" });
+    const result = await spawnAsync(this._command, this._args, { cwd: this._cwd, encoding: "utf-8", nostdout: true });
     if (result.error || result.status) {
       logger.notice("cd " + this._cwd);
       let cmd = this._args.join(" ");
--- src/utils/ChildProcess.ts
+++ src/utils/ChildProcess.ts
@@ -15,13 +15,30 @@ import { Logger } from "@/logger";
 
 const logger = Logger.create(import.meta.url);
 
-type Result = {
-  status: number;
+interface SpawnAsyncOptions {
+  cwd?: string;
+  encoding?: BufferEncoding;
+  env?: any;
+  nostdout?: boolean;
+  extra?: {
+    verbose?: boolean;
+    output?: string;
+  };
 };
 
-export function spawnAsync(command: string, args: string[], options?: any): Promise<Result> {
+interface SpawnAsyncReturns {
+  status: number;
+  stdout: string;
+  stderr: string;
+  output: string;
+  error?: Error | undefined;
+};
+
+export function spawnAsync(command: string, args: string[], options?: SpawnAsyncOptions): Promise<SpawnAsyncReturns> {
   let fd = null;
   let verbose = false;
+  const encoding = options?.encoding;
+
   if (options && options.extra) {
     if (options.extra.verbose)
       verbose = true;
@@ -33,24 +50,47 @@ export function spawnAsync(command: string, args: string[], options?: any): Prom
       fd = fs.openSync(logfile, "w+", 0o666);
     }
   }
+
   return new Promise((resolve, reject) => {
-    if (fd || verbose) {
-      verbose && logger.notice([ path.basename(command), ...args ].join(" "));
-      fd && fs.writeSync(fd, JSON.stringify({command, args, options }, null, 2) + "\n");
+    verbose && logger.notice([ path.basename(command), ...args ].join(" "));
+
+    if (fd) {
+      fs.writeSync(fd, JSON.stringify({command, args, options }, null, 2) + "\n");
     }
+
+    const stdout: Buffer[] = [];
+    const stderr: Buffer[] = [];
+    const output: Buffer[] = [];
+
     const exec = spawn(command, args, options);
-    exec.stdout.on("data", (data) => {
-      process.stdout.write(data);
-      fd && fs.writeSync(fd, data);
-    });
-    exec.stderr.on("data", (data) => {
-      process.stderr.write(data);
-      fd && fs.writeSync(fd, data);
-    });
-    exec.on("close", (status: number) => {
-      fd && fs.closeSync(fd);
-      resolve({status});
-    });
+
+    if (fd) {
+      exec.stdout.addListener("data", chunk => fs.writeSync(fd, chunk));
+      exec.stderr.addListener("data", chunk => fs.writeSync(fd, chunk));
+      exec.addListener("close", () => fs.closeSync(fd));
+    }
+    else {
+      exec.stdout.addListener("data", chunk => {
+        stdout.push(chunk);
+        output.push(chunk);
+      });
+      exec.stderr.addListener("data", chunk => {
+        stderr.push(chunk);
+        output.push(chunk);
+      });
+    }
+
+    if (!options?.nostdout) {
+      exec.stdout.addListener("data", chunk => process.stdout.write(chunk));
+      exec.stderr.addListener("data", chunk => process.stderr.write(chunk));
+    }
+
+    exec.addListener("close", (status: number) => resolve({
+      status,
+      stdout: Buffer.concat(stdout).toString(encoding),
+      stderr: Buffer.concat(stderr).toString(encoding),
+      output: Buffer.concat(output).toString(encoding),
+    }));
   });
 }
 
